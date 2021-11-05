95bf172b2ec213cfd8a2f56f256b5bd838021077
jorgemeyrelles <jotaengpuc@gmail.com>
2021-11-05 11:46:45 -0300

    feat: requisito 6

--- string_calculator.js
+++ string_calculator.js
@@ -1,13 +1,18 @@
+const UserException = (value) => {
+  if (value === 1) throw new 'separador no fim';
+  if (value === 2) throw new 'numero negativo na string';
+};
+
 const add = (string) => {
   if (string.length === 0) {
     return 0;
   } else if (string[string.length - 1] === ',') {
-    throw new 'separador no fim';
+    return UserException(1);
   }
-  // console.log((string.match(/-?\d+/g).map((e) => Number(e) < 0)));
-  // string.match(/-?\d+/g).map((e) => {
-  //   if (Number(e) < 0) throw new erro('numero negativo na string');
-  // });
+  // regex /-?\d+/g retorna todos os numeros inclusive os negativos
+  string.match(/-?\d+/g).map((e) => {
+    if (Number(e) < 0) return UserException(2);
+  });
   // esta expressão regular identifica "," e "\n"
   return (string.split(/[^0-9]+/))
     .filter((e) => e[0] !== undefined).reduce((a, b) => parseInt(a) + parseInt(b), 0);
