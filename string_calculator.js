786afcca2a18fa594ad740dbb1ba7eb9b4407138
jorgemeyrelles <jotaengpuc@gmail.com>
2021-11-10 11:07:45 -0300

    fix: func validateImput, TypeError e refatoracao

--- string_calculator.js
+++ string_calculator.js
@@ -1,21 +1,30 @@
-const UserException = (value) => {
-  if (value === 1) throw new 'separador no fim';
-  if (value === 2) throw new `Negative number(s) not allowed`;
-};
+const error1 = 'Not to allow a separator at the end';
+const error2 = 'Negative number(s) not allowed'
+
+// validação
+const validateInput = (string) => {
+  const arr = [];
+  // esta expressão regular identifica separadores mesmo antes de "\n"
+  if ((/[^\d]+/).test(string[string.length - 1])) {
+    arr.push(error1);
+  }
+  // regex /-?\d+/g retorna todos numeros inteiros inclusive os negativos
+  string.match(/-?\d+/g).map((e, i) => {
+    if (Number(e) < 0) {
+      arr.push(`${error2}: ${Number(e)}, found at position ${i}`);
+    }
+  });
+  // typeError para enviar os valores válidos
+  if (arr.length > 0) throw new TypeError(arr.join('\n'));
+}
 
 const add = (string) => {
-  if (string.length === 0) {
-    return 0;
-  } else if (string[string.length - 1] === ',') {
-    return UserException(1);
-  }
-  // regex /-?\d+/g retorna todos os numeros inclusive os negativos
-  string.match(/-?\d+/g).map((e) => {
-    if (Number(e) < 0) return UserException(2);
-  });
-  // esta expressão regular identifica "," e "\n"
+  // identifica se recebe string diferente de null
+  if (string.length === 0) return 0;
+  //inicia validação
+  validateInput(string);
+  // ignora numeros maiores de 1000
   const lessThen = string.match(/-?\d+/g).filter((e) =>  e < 1000);
-  // console.log(lessThen.reduce((a, b) => parseInt(a) + parseInt(b), 0));
   return lessThen.reduce((a, b) => parseInt(a) + parseInt(b), 0);
 };
 
--- string_calculator.test.js
+++ string_calculator.test.js
@@ -27,25 +27,29 @@ describe('String calculator', () => {
   });
   it('Recebe string com separador no fim e retorna erro', () => {
     const strOne = '1,2,';
-    expect(() => stringFile.add(strOne)).toThrow('separador no fim');
+    expect(() => stringFile.add(strOne)).toThrow('Not to allow a separator at the end');
   })
-  it('Recebe string com diferentes tipos de separador', () => {
+  it('Recebe string com diferentes tipos de separador: retorna 4', () => {
     const strOne = '//;\n1;3';
-    const strSnd = '//sep\n2sep5';
-    const strTrd = '//|\n1|2,3';
     expect(stringFile.add(strOne)).toBe(4);
+  });
+  it('Recebe string com diferentes tipos de separador: retorna 7', () => {
+    const strSnd = '//sep\n2sep5';
     expect(stringFile.add(strSnd)).toBe(7);
+  });
+  it('Recebe string com diferentes tipos de separador: retorna 6', () => {
+    const strTrd = '//|\n1|2,3';
     expect(stringFile.add(strTrd)).toBe(6);
   });
   it('Recebe string com separadores e não aceita numeros negativos', () => {
     const strOne = '1,-3';
     // stringFile.add(strOne);
-    expect(() => stringFile.add(strOne)).toThrow(`Negative number(s) not allowed`);
+    expect(() => stringFile.add(strOne)).toThrow(`Negative number(s) not allowed: ${-3}`);
   });
   it('Recebe string com separadores diferentes e numeros negativos', () => {
-    const strOne = '//|\n1|2,-3';
+    const strOne = '//|\n1|2,-3,';
     // stringFile.add(strOne);
-    expect(() => stringFile.add(strOne)).toThrow(`Negative number(s) not allowed`);
+    expect(() => stringFile.add(strOne)).toThrow(`Not to allow a separator at the end\nNegative number(s) not allowed: ${-3}, found at position ${2}`);
   });
   it('Ignora valores acima de 1000', () => {
     const strOne = '1001, 2';
